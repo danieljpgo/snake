60f7fa933ff05f3e2405d4048b806187472577ac
Daniel Jorge <djpgonzaga@gmail.com>
2020-10-18 18:22:48 -0300

    fix: cleaning create-react-app

---
+++ src/app/components/Table/Cell/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cell = () => {
+  return (
+    <div style={{ borderRadius: 4, backgroundColor: '#000000' }} >
+
+    </div>
+  );
+}
+
+export default Cell;
---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Table from './components/Table';
+import './styles.css';
+
+const App = () => {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: '100vh' }}>
+      <Table />
+    </div>
+  );
+}
+
+export default App;
